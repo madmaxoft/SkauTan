07977b3b9a0654ffd232fb55932208d60ba47ea0
Mattes D <github@xoft.cz>
2018-02-08 10:45:46 +0100

    Appveyor: Added TagLib download

--- appveyor-build.cmd
+++ appveyor-build.cmd
@@ -6,17 +6,33 @@ if not exist c:\projects\lib\include\avformat\avformat.h (
 	mkdir c:\projects\lib
 	curl -o c:\projects\lib\libav.zip https://ffmpeg.zeranoe.com/builds/win64/dev/ffmpeg-3.4.1-win64-dev.zip
 	if errorlevel 1 (
-		echo Download unsuccessfull
+		echo Download unsuccessful
 		exit /b 1
 	)
 	7z x c:\projects\lib\libav.zip -oc:\projects\lib
 	if errorlevel 1 (
-		echo Unzip unsuccessfull
+		echo Unzip unsuccessful
 		exit /b 1
 	)
 	move c:\projects\lib\ffmpeg-3.4.1-win64-dev\include c:\projects\lib
 	move c:\projects\lib\ffmpeg-3.4.1-win64-dev\lib c:\projects\lib
 )
+
+rem Download the precompiled TagLib:
+if not exist c:\projects\lib\include\taglib\tag.h (
+	echo Downloading precompiled TagLib libraries...
+	curl -o c:\projects\lib\taglib.7z http://xoft.cz/taglib-msvc2015-x64.7z
+	if errorlevel 1 (
+		echo Download unsuccessful
+		exit /b 1
+	)
+	7z x c:\projects\lib\taglib.7z -oc:\projects\lib
+	if errorlevel 1 (
+		echo Unzip unsuccessful
+		exit /b 1
+	)
+)
+echo Libraries checked, compiling now...
 
 set LIB=%LIB%;c:\projects\lib\lib
 set INCLUDE=%INCLUDE%;c:\projects\lib\include
