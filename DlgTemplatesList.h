234d8ccaed3e73aed892cd04c5b0288d0bec707a
Mattes D <github@xoft.cz>
2018-05-08 23:25:21 +0200

    DlgTemplateList: Allow editting in tables and by dblclick.

--- DlgTemplatesList.h
+++ DlgTemplatesList.h
@@ -16,6 +16,7 @@
 // fwd:
 class Database;
 class MetadataScanner;
+class QTableWidgetItem;
 namespace Ui
 {
 	class DlgTemplatesList;
@@ -55,12 +56,23 @@ private:
 	/** The Qt-managed UI. */
 	std::unique_ptr<Ui::DlgTemplatesList> m_UI;
 
+	/** Set to true if the UI is updating internally; false when the changes come from the user. */
+	bool m_IsInternalChange;
+
 
 	/** Returns the template that is at the specified row in the table. */
 	TemplatePtr templateFromRow(int a_Row);
 
-	/** Updates the specified row in the template list with the current template data. */
+	/** Returns the template item that is at the specified row in the table.
+	a_Template is the template currently being displayed in the Items table.
+	a_ItemRow is the row number of the item in the Items table. */
+	Template::ItemPtr templateItemFromRow(TemplatePtr a_Template, int a_ItemRow);
+
+	/** Updates the specified row in the template table with the current template data. */
 	void updateTemplateRow(int a_Row, const Template & a_Template);
+
+	/** Updates the specified row in the template item table with the current template item data. */
+	void updateTemplateItemRow(int a_Row, const Template::Item & a_Item);
 
 	/** Exports the selected templates to the specified file. */
 	void exportTemplatesTo(const QString & a_FileName);
@@ -88,6 +100,17 @@ protected slots:
 
 	/** The selection in the template list has changed. */
 	void templateSelectionChanged();
+
+	/** The template in the table has changed, either by us (m_IsInternalChange) or by the user.
+	If the change was by the user, save the new contents to the template. */
+	void templateChanged(QTableWidgetItem * a_Item);
+
+	/** The template item in the table has changed, either by us (m_IsInternalChange) or by the user.
+	If the change was by the user, save the new contents to the template item. */
+	void itemChanged(QTableWidgetItem * a_Item);
+
+	/** Opens the template item editor for the template item at the specified index. */
+	void editTemplateItemAt(const QModelIndex & a_Index);
 
 	/** The user clicked the Export button.
 	Shows a file selection dialog, then exports selected templates into that file. */
